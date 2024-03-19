9b0fdcdfe92572042d005a79012d44e0bb2c756d
Carlos Silva <c.edusilva@yahoo.com>
2024-03-19 02:26:48 -0300

    Adicionar arquivos de projeto.

---
+++ ApiUsuario/Model/User.cs
@@ -0,0 +1,27 @@
+﻿namespace ApiUsuario.Model
+{
+    public class User
+    {
+        public int id { get; private set; }
+
+        public string name { get; set; }
+
+        public int cpf { get; set; }
+
+        public DateTime birthdayData { get; set; }
+
+        public int numberPhone { get; set; }
+
+        public string email { get; set; }
+
+        public string password { get; set; }
+
+        public string address { get; set; }
+
+        public string numberHome { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+
+    }
+}
